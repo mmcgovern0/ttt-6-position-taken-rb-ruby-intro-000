27f6b949efc8b87cfacdbc21c8defc222fcb5b3b
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-12 17:52:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  taken = nill
-  if (board[index] == " " || board[index] == "" || board[index] == nill)
+  taken = nil
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else 
     taken = true
